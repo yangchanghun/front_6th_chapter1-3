d1f22ac663b663d64b6d4825fbc6c7d2138e9223
yangchanghun <gory4848@naver.com>
2025-07-24 23:49:44 +0900

    deepEquals.ts구현

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,27 @@
-export const deepEquals = (a: unknown, b: unknown) => {
-  return a === b;
+export const deepEquals = (a: unknown, b: unknown): boolean => {
+  if (a === b) return true;
+
+  if (Array.isArray(a) && Array.isArray(b) && a.length === b.length) {
+    return a.every((v, i) => deepEquals(v, b[i]));
+  }
+
+  if (
+    typeof a === "object" &&
+    typeof b === "object" &&
+    a !== null &&
+    b !== null &&
+    !Array.isArray(a) &&
+    !Array.isArray(b)
+  ) {
+    const objA = a as Record<string, unknown>;
+    const objB = b as Record<string, unknown>;
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length) return false;
+
+    return keysA.every((key) => keysB.includes(key) && deepEquals(objA[key], objB[key]));
+  }
+
+  return false;
 };
