10e3af83b87082751fe67351950ff194ad3f135c
yangchanghun <gory4848@naver.com>
2025-07-24 23:56:25 +0900

    deepMemo.ts 구현

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,6 @@
 import type { FunctionComponent } from "react";
-
+import { memo } from "./memo";
+import { deepEquals } from "../equals";
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  return memo(Component, deepEquals);
 }
