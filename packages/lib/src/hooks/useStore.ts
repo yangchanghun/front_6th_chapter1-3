a9d37c9e7168c4787d984d89526430177a351f36
yangchanghun <gory4848@naver.com>
2025-07-24 23:58:24 +0900

    useStore.ts 구현

--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -8,6 +8,12 @@ const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
   // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고 가져오는 훅을 구현해보세요.
+
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(store.getState());
+
+  return useSyncExternalStore(
+    store.subscribe, // 상태 변경 구독
+    () => shallowSelector(store.getState()), // 현재 상태에서 필요한 부분만 추출
+  );
 };
+// return { getState, dispatch, subscribe };
