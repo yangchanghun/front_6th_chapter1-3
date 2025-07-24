a126d94de2be4142f99725189ff148706197ae42
yangchanghun <gory4848@naver.com>
2025-07-24 23:53:24 +0900

    useAutoCallback.ts 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,10 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef(fn);
+  // 초기값으로 fn 설정해줬는데 왜 또 아래에서 fn을 할당하지
+  ref.current = fn;
+  return useCallback((...args) => {
+    return ref.current(...args);
+  }, []);
 };
